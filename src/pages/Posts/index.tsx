d507592b979f26d413987bbcbc00fa772bb18c9a
crlne <carol.lages@outlook.com>
2021-04-07 15:08:32 -0300

    pages

---
+++ src/pages/Posts/index.tsx
@@ -0,0 +1,10 @@
+export default function Post() {
+    return (
+        <main>
+            <div>
+                <h1>Titulo</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adip</p>
+            </div>
+        </main>
+    );
+}
